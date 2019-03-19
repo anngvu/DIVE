400832a14c77c2ec1773b2ce7fd785043848d849
ANV <avu@coh.org>
2019-03-19 13:01:43 -0700

    conditional display of geneVUI

--- R/multiVApp.R
+++ R/multiVApp.R
@@ -1,5 +1,5 @@
 #' Shiny app UI for multi-views
-#' 
+#'
 #' Assembles the UI of various module components, i.e. \code{\link{multiVUI}}, \code{\link{geneVUI}},
 #' into a working one-page application
 #'
@@ -7,31 +7,32 @@
 #' @param CSS Optional, location to an alternate CSS stylesheet to change the look and feel of the app.
 #' @export
 multiVAppUI <- function(id, CSS = system.file("www/", "app.css", package = "DIVE")) {
-  
+
   ns <- NS(id)
   fluidPage(theme = shinythemes::shinytheme("lumen"), includeCSS(CSS),
             multiVCtrlUI(ns("ctrl")),
-            geneVUI(ns("gene")),
+            # only show geneVUI if one or more highthroughput datasets are selected
+            conditionalPanel(condition = paste0("input['", ns("ctrl-dataset"), "']"), geneVUI(ns("gene"))),
             div(id = "displaytrack")
   )
 }
 
 #' Shiny app server for multi-views
-#' 
+#'
 #' Assembles the logic of various module components, i.e. \code{\link{multiV}}, \code{\link{geneV}},
 #' into a working one-page application
 #'
 #' @param input,output,session Standard \code{shiny} boilerplate.
 #' @export
 multiVApp <- function(input, output, session,
-                      HDATA = list("genomics1" = xm_t, "px1" = px1_t, "px2" = px2_t), 
-                      CDATA = cdata, 
+                      HDATA = list("genomics1" = xm_t, "px1" = px1_t, "px2" = px2_t),
+                      CDATA = cdata,
                       CHOICES = gene_symbols) {
-  
-  selected <- callModule(geneV, "gene", choices = CHOICES) # controls selection for all multiVUIs
-  
+
   view <- callModule(multiVCtrl, "ctrl", hdlist = HDATA)
-  
+
+  selected <- callModule(geneV, "gene", choices = CHOICES) # controls selection for all multiVUIs
+
   # each dataset gets its own track (row), served by its own multiVUI module
   observeEvent(view(), {
     trackID <- session$ns(names(view()))
@@ -43,7 +44,7 @@ multiVApp <- function(input, output, session,
       removeUI(selector = paste0("#", trackID))
     }
   })
-  
+
 }
 
 multiVAppRun <- function() {
