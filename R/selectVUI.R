cf60a939e6e4c257a0e3de2770ea965adcb3e04b
ANV <avu@coh.org>
2019-03-21 09:26:19 -0700

    changes to selectVUI

--- R/selectVUI.R
+++ R/selectVUI.R
@@ -7,9 +7,8 @@
 selectVUI <- function(id) {
   ns <- NS(id)
   tags$div(id = "selectVUI",
-          div(class = "forceInline", uiOutput(ns("select"))),
-          div(class = "forceInline", br(), actionButton(ns("go"), "select"))
-          )
+           div(class = "forceInline", uiOutput(ns("select")))
+  )
 }
 
 #' Shiny module server for selecting columns from a data table
@@ -18,21 +17,22 @@ selectVUI <- function(id) {
 #'
 #' @param input,output,session Standard \code{shiny} boilerplate.
 #' @param data A data table.
+#' @param key A key column that is kept for every selected subset. Defaults to "ID".
 #' @param selected Optional, initial selection.
 #' @return The subsetted data table.
 #' @export
 selectV <- function(input, output, session,
-                  data, selected = NULL) {
+                    data, key = "ID", selected = NULL) {
 
-   output$select <- renderUI({
-     selectizeInput(session$ns("var"), "Variable",
-                    choices = names(data), selected = selected,
-                    options = list(maxItems = 50))
-   })
+  output$select <- renderUI({
+    selectizeInput(session$ns("var"), "Variable",
+                   choices = names(data)[names(data) != key], selected = selected,
+                   options = list(maxItems = 50))
+  })
 
-  V <- eventReactive(input$go, {
-    data[, c("ID", input$var), with = F]
-  }, ignoreInit = F)
+  V <- eventReactive(input$var, {
+    data[, c(key, input$var), with = F]
+  })
 
   return(V)
 
