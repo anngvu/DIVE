7a25285d974a38b70333334d8c76ed553786945d
ANV <avu@coh.org>
2021-04-14 15:34:26 -0700

    v2 of matrixApp with matrixCtrl

---
+++ R/matrixApp2.R
@@ -0,0 +1,134 @@
+#' Shiny app UI for interactive matrix (v2)
+#'
+#' See matrixCtrl2UI
+#'
+#' @param id Character ID for specifying namespace, see \code{shiny::\link[shiny]{NS}}.
+#' @param CSS Optional, location to an alternate CSS stylesheet to change the look and feel of the app.
+#' @import shiny
+#' @import magrittr
+#' @export
+matrixApp2UI <- function(id, CSS = system.file("www/", "app.css", package = "DIVE")) {
+  ns <- NS(id)
+  ns.graph <- paste(ns("graph"), "asNetwork", sep = "-")
+  ns.matrix <- paste(ns("matrix"), "matrixMain", sep = "-")
+  fluidPage(theme = shinythemes::shinytheme("paper"),
+            if(!is.null(CSS)) includeCSS(CSS),
+            # Filter controls and data input
+            fluidRow(class = "top-panel matrixApp2UI-panel",
+                     column(12,
+                            div(class = "input-panel-flush",
+                              div(style = "padding-left: 50px;",
+                                  shinyWidgets::materialSwitch(ns("lookup"), label = "DETAILED SEARCH")),
+                              absolutePanel(id = ns("filter"),
+                                            conditionalPanel("input.lookup", ns = ns,
+                                              dataHelperUI("metahelper", oneway = TRUE))
+                                            )
+                            ),
+                            div(class = "input-panel", matrixCtrl2UI(ns("ctrl"))),
+                            div(class = "input-panel", br(), actionButton(ns("upload"), "Upload my data"))
+                            )
+            ),
+            tags$div(id = ns("view-switch"), class = "btn-group", style = "margin-bottom: 20px;",
+                     actionButton("viewgraph", "Graph view") %>%
+                       tagAppendAttributes(class = "btn-sm", onclick = sprintf("{ $('#%s').hide();
+                                                 $('#%s').trigger('hidden');
+                                                 $('#%s').show();
+                                                 $('#%s').trigger('shown');
+                                                 $(this).addClass('active').siblings().removeClass('active'); }", ns.matrix, ns.matrix, ns.graph, ns.graph)),
+                     actionButton("viewmatrix", "Matrix view") %>%
+                       tagAppendAttributes(class = "btn-sm active", onclick = sprintf("{ $('#%s').hide();
+                                                 $('#%s').trigger('hidden');
+                                                 $('#%s').show();
+                                                 $('#%s').trigger('shown');
+                                                 $(this).addClass('active').siblings().removeClass('active'); }", ns.graph, ns.graph, ns.matrix, ns.matrix))
+            ),
+            tags$div(style = "position:relative; display: flex; align-items: flex-start;",
+                     matrixMainUI(ns("matrix"), style = "flex: 4 0 70vw;"),
+                     matrixAsNetworkUI(ns("graph"), height = "1000px", style = "flex: 4 0 70vw; display: none;"),
+                     dualDrilldownUI(ns("dd"))
+            )
+  )
+}
+
+#' Shiny app server for interactive matrix (v2)
+#'
+#' See matrixCtrl2UI
+#'
+#' @inheritParams dataUploadServer
+#' @inheritParams matrixCtrlServer
+#' @inheritParams matrixMainServer
+#' @inheritParams matrixAsNetworkServer
+#' @inheritParams dualDrilldownServer
+#' @param metafilter A reactive filter such as from \code{dataHelper}, used to filter upon the matrix.
+#' @import shiny
+#' @export
+matrixApp2Server <- function(id,
+                            M, N, P,
+                            cdata,
+                            metafilter = reactive({ }),
+                            factorx, colorby,
+                            colorscales = list(default = list(colorscale_named(pal = "RdBu"), zmin = -1, zmax = 1)),
+                            informd = system.file("info/interactive_matrix.Rmd", package = "DIVE"),
+                            appdata = NULL) {
+
+  moduleServer(id, function(input, output, session) {
+
+    observeEvent(input$upload, {
+      showModal(modalDialog(title = "Upload my data",
+                            dataUploadUI(session$ns("upload"), label = NULL),
+                            includeMarkdown(informd),
+                            footer = modalButton("Cancel")
+      ))
+    })
+
+    upload <- dataUploadServer("upload",
+                               removable = T,
+                               checkFun = checkDataUpload,
+                               appdata = appdata,
+                               checkappdata = T)
+
+    filtervals <- reactiveValues(N = NULL, P = NULL)
+
+    mdata <- matrixCtrl2Server("ctrl",
+                               M = M, N = N, P = P,
+                               cdata = cdata,
+                               metafilter = metafilter,
+                               newdata = upload,
+                               filtervals = filtervals)
+
+    src1 <- matrixMainServer("matrix",
+                             mdata = mdata,
+                             colorscales = colorscales)
+
+    src2 <- matrixAsNetworkServer("graph",
+                                  mdata,
+                                 filtervals,
+                                  background = "#201037",
+                                  .nodes = list(size = 10, color = list(background = "lemonchiffon", border = "yellow", highlight = "white"),
+                                                font = list(color = "white")),
+                                  .edges = list(color = "yellow"),
+                                  .options = list(highlightNearest = TRUE,
+                                                  nodesIdSelection = list(enabled = TRUE)),
+                                  randomSeed = 42)
+
+    dualDrilldownServer("dd",
+                        cdata = reactive(mdata$cdata),
+                        colorby = colorby,
+                        src1 = src1, src2 = src2)
+
+    })
+}
+
+#' Launch Shiny app for exploration of relationships in annotated data with an interactive matrix
+#'
+#' Wrapper to launch app at console
+#'
+#' @param ns Namespace of app module.
+#' @param ... Arguments passed to \code{\link{matrixApp2Server}}.
+#' @export
+matrixApp2 <- function(ns, ...) {
+  ui <- matrixApp2UI(ns)
+  server <- matrixApp2Server(ns, ...)
+  shinyApp(ui, server)
+}
+
---
+++ R/matrixCtrl2.R
@@ -0,0 +1,245 @@
+#' Shiny module UI for matrix view
+#'
+#' Interactive controls of matrix view
+#'
+#' @param id Character ID for specifying namespace, see \code{shiny::\link[shiny]{NS}}.
+#' @param minN Minimum number for numeric input filter.
+#' @export
+matrixCtrl2UI <- function(id, minN = 5) {
+  ns <- NS(id)
+  tags$div(id = ns("matrixCtrlUI"), class = "matrixCtrlUI-panel",
+           div(class = "ui-inline",
+               div(class = "ui-inline",
+                   selectizeInput(ns("optrow"),
+                                  label = HTML("<strong>Selected nodes</strong>"),
+                                  choices = "", selected = NULL, multiple = T, width = "500px",
+                                  options = list(placeholder = paste("select to view"))
+                                  )
+               )
+           ),
+           div(class = "ui-inline",
+               conditionalPanel("input.optrow != ''", ns = ns,
+                 div(class = "ui-inline", br(), icon("arrow-right", class = "fa-3x")),
+                 div(class = "ui-inline",
+                     selectizeInput(ns("optcol"),
+                                    label = HTML("<strong>Selected connected nodes</strong>"),
+                                    choices = "", multiple = T, width = "400px"))
+               )
+           ),
+           div(class = "ui-inline",
+               conditionalPanel("input.optcol == ''", ns = ns,
+                 numericInput(ns("minN"),
+                              label = "mininum N",
+                              value = 5, min = minN, step = 1, width = "80px")
+                 )
+               ),
+           div(class = "ui-inline", br(),
+               conditionalPanel("input.optcol == ''", ns = ns,
+                 checkboxInput(ns("cutoffP"),
+                               label = "P < 0.05 ",
+                               value = FALSE, width = "80px")
+                 )
+               )
+           )
+}
+
+#' Shiny module server functions to control matrix view
+#'
+#' Create a matrix view that can be visualized with appropriate plotting modules
+#'
+#' The module can integrate several types of interactions that control
+#' the view of a matrix. It handles recalculation of the view
+#' and passes on the data to be visualized with
+#' \code{\link{matrixMainServer}} or \code{\link{matrixAsNetworkServer}}.
+#' The base UI implements drop-down selection for basic subsetting of matrix indices
+#' and threshold-type filters, and integrates with other modules providing
+#' new data input or specialized selection interface. For example, for some data
+#' a map widget might provide a better selection interface than a simple
+#' drop-down, thus the map widget module could be implemented separately
+#' and its result communicated as \code{metafilter}.
+#'
+#' @param id Character ID for specifying namespace, see \code{shiny::\link[shiny]{NS}}.
+#' @param M A data matrix, e.g. a correlation matrix, which must have row and column names.
+#' @param N A matrix of the same dimensions as `M` to be used as a filter layer, e.g. sample size.
+#' @param P A matrix of the same dimensions as `M` to be used as a filter layer, e.g. p-values.
+#' @param cutoffP A cutoff value for `P` to be used as default for filtering.
+#' @param cdata The data used for generating the matrix,
+#' necessary for allowing user-uploaded data to be integrated into mutable `M`.
+#' @param metafilter Optional reactive data (table) where first column contains rows to be selected
+#' and metadata attributes as additional columns.
+#' @param newdata Optional reactive data (table) such as a user upload passed in by
+#' the \code{\link{dataUploadServer}} module or from some other component,
+#' and which is suitable for merging with \preformatted{cdata} to calculate a new `M`.
+#' @param filtervals Optional reactive object for storing filter values that need to be communicated to other modules.
+#' @return Reactive values in \code{mstate} object that keeps track of visible matrices
+#' and selected metadata and is used by the associated plotting module.
+#' @export
+matrixCtrl2Server <- function(id,
+                              M = NULL, N = NULL, P = NULL,
+                              cutoffP = 0.05,
+                              cdata = NULL,
+                              metafilter = reactive({}),
+                              newdata = reactive({}),
+                              filtervals = NULL) {
+
+  moduleServer(id, function(input, output, session) {
+
+    mstate <- reactiveValues(M = M, N = N, P = P,
+                             cdata = cdata,
+                             newdata = NULL,
+                             rowmeta = NULL, colmeta = NULL, filM = M)
+    request <- reactiveValues(optrow = NULL)
+
+    # Some applications need filter values to be passed on in addition to mstate values
+    # If so, a reactive value object should be passed into filtervals params
+    obs_filter <- observe({
+      filtervals$N <- input$minN
+      filtervals$P <- if(input$cutoffP) cutoffP else NULL
+    }, suspended = T)
+
+    if(!is.null(filtervals)) obs_filter$resume()
+
+    #-- Initialize UI -------------------------------------------------------------------------------#
+
+    updateSelectizeInput(session, "optrow", choices = rownames(M))
+
+    #-- Functions -----------------------------------------------------------------------------------#
+
+    # Return a filtered matrix (filM)
+    filterUpdate <- function(M = mstate$M, N = mstate$N, P = mstate$P,
+                             minN, cutoffP = NULL,
+                             optrows = rownames(M), optcols = colnames(M)) {
+      m <- M[optrows, optcols, drop = F]
+      n <- N[optrows, optcols, drop = F]
+      m[n < minN] <- NA # gray out entries in M that does not meet minN
+      deadrows <- apply(m, 1, function(x) all(is.na(x))) # hide "dead pixels" (no values > minN)
+      deadcols <- apply(m, 2, function(x) all(is.na(x)))
+      m <- m[!deadrows, !deadcols, drop = F]
+      if(!is.null(P) && !is.null(cutoffP) && all(dim(m) > 0)) {
+        p <- P[rownames(m), colnames(m)]
+        m[p > cutoffP] <- NA
+      }
+      return(m)
+    }
+
+
+    #-- Row selection -------------------------------------------------------------------------------#
+
+    # The optrow selection controls which rows are displayed; by default, choices are all rows in M.
+    # The selected choices can be updated regularly by 1) external filter modules, 2) queryString,
+    # or 3) when new data is uploaded, all of which keeps selection request in request$optrow.
+    observeEvent(request$optrow, {
+      selected <- request$optrow
+      updateSelectizeInput(session, "optrow",
+                           # label = sprintf("<strong>Available %s</strong>", input$optrowgroup), # https://github.com/rstudio/shiny/issues/1140
+                           selected = selected)
+      request$optrow <- NULL
+    }, ignoreInit = TRUE) # because requires initial updateSelectInput to populate optrowgroup
+
+
+    # Updating visible matrix rows according to selected row opt
+    observeEvent(input$optrow, {
+      if(!length(input$optrow)) {
+        mstate$filM <- filterUpdate(minN = input$minN, cutoffP = if(input$cutoffP) cutoffP) # mstate$M
+        mstate$rowmeta <- mstate$colmeta <- NULL
+      } else {
+        optrows <- input$optrow
+        mstate$filM <- filterUpdate(minN = input$minN, cutoffP = if(input$cutoffP) cutoffP, optrows = optrows)
+        # meta is _optionally_ returned by metafilter as the second column or list element
+        if(length(metafilter()) > 1) mstate$rowmeta <- metafilter()[[2]][metafilter()[[1]] %in% rownames(mstate$filM)]
+      }
+    }, ignoreInit = TRUE, ignoreNULL = F, priority = 10) # matrix update should run before optcol update (see below)
+
+    #-- Column selection --------------------------------------------------------------------------#
+
+    # Populate optcol according to current rows selected
+    observeEvent(input$optrow, {
+      optcols <- colnames(mstate$filM)
+      updateSelectizeInput(session, "optcol", choices = optcols)
+    })
+
+
+    # Updating matrix according to selected col opt
+    observeEvent(input$optcol, {
+      if(!length(input$optcol)) {
+        mstate$filM <- filterUpdate(minN = input$minN, cutoffP = if(input$cutoffP) cutoffP,
+                                    optrows = rownames(mstate$filM), optcols = colnames(mstate$M))
+        mstate$colmeta <- NULL
+      } else {
+        optcols <- input$optcol
+        mstate$filM <- filterUpdate(minN = input$minN, cutoffP = if(input$cutoffP) cutoffP,
+                                    optrows = rownames(mstate$filM), optcols = optcols)
+        # Metadata for cols requires looking up current col ids with db
+      }
+    }, ignoreNULL = F, ignoreInit = TRUE)
+
+    #-- N filter ----------------------------------------------------------------------------------#
+
+    # Apply minimum N
+    observeEvent(input$minN, {
+
+        optrows <- rownames(mstate$filM)
+        optcols <- colnames(mstate$filM)
+        mstate$filM <- filterUpdate(minN = input$minN,
+                                    cutoffP = if(input$cutoffP) cutoffP,
+                                    optrows = optrows, optcols = optcols)
+        #if(length(optrows) > 1) mstate$rowmeta <- optrows[[2]][optrows[[1]] %in% rownames(mstate$filM)]
+        #if(length(optcols) > 1) mstate$colmeta <- optcols[[2]][optcols[[1]] %in% colnames(mstate$filM)]
+
+    })
+
+    #-- P filter ----------------------------------------------------------------------------------#
+
+    observeEvent(input$cutoffP, {
+      if(input$cutoffP) {
+        mstate$filM <- filterUpdate(minN = input$minN, cutoffP = cutoffP,
+                                    optrows = rownames(mstate$filM), optcols = colnames(mstate$filM))
+      } else {
+        mstate$filM <- filterUpdate(minN = input$minN, cutoffP = NULL,
+                                    optrows = rownames(mstate$filM), optcols = colnames(mstate$filM))
+      }
+    }, ignoreInit = TRUE)
+
+    #-- Metafilter handling -----------------------------------------------------------------------#
+
+    # If metafilter changes, put new rows in request
+    # (row indices should always be in first column/first list element)
+    observeEvent(metafilter(), {
+      request$optrow <- metafilter()[[1]]
+    }, ignoreInit = TRUE)
+
+    #-- New data handling -------------------------------------------------------------------------#
+
+    # Update M, cdata, and options given newdata
+    # Does not ignore NULL --> reset to default when newdata is removed
+    observeEvent(newdata(), {
+      if(is.null(newdata())) {
+        updateSelectizeInput(session, "optrow", selected = character(0))
+        mstate$cdata <- cdata
+        mstate$filM <- mstate$M <- M
+        mstate$N <- N
+        mstate$rowmeta <- mstate$colmeta <- NULL
+      } else {
+        tryCatch({
+          withProgress(message = "adding new data", expr = {
+            newDT <- newdata()
+            names(newDT) <- make.names(names(newDT))
+            cdata2 <- merge(cdata, newDT, by = "ID", all.x = T, all.y = F)
+            updated <- suppressWarnings(data2cor(cdata2))
+            mstate$cdata <- cdata2
+            mstate$filM <- mstate$M <- updated$M
+            mstate$N <- updated$N
+            mstate$P <- updated$P
+            mstate$rowmeta <- mstate$colmeta <- NULL
+            request$optrow <- names(newDT) # trigger view update to focus on new uploaded data
+          })
+        }, error = function(e) meh(error = e) )
+      }
+    }, ignoreInit = T, ignoreNULL = F)
+
+
+    # --------------------------------------------------------------------------------------------#
+
+    return(mstate)
+  })
+}
